8dc7d8d6ea5394532eb392d58b38a6fccaf1c31a
Alexander Shishenko <alex@shishenko.com>
2022-10-09 06:53:30 +0300

    Fixup Darwin builds

--- src/sys/darwin/mod.rs
+++ src/sys/darwin/mod.rs
@@ -1,15 +1,3 @@
-pub use handle::InterfaceHandleExt;
-pub(crate) use metadata::Metadata;
-pub use metadata::MetadataExt;
-use std::collections::HashSet;
+pub use handle::InterfaceExt;
 mod handle;
-mod metadata;
 pub(crate) mod scinterface;
-
-pub(crate) fn list_interfaces() -> Vec<crate::InterfaceHandle> {
-    nix::net::if_::if_nameindex()
-        .unwrap()
-        .iter()
-        .map(|a| crate::InterfaceHandle::from_index_unchecked(a.index()))
-        .collect()
-}
